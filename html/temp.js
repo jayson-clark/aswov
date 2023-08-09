95a6c3e88d526fb448c4eca0fe4e45bba2ad8cdb
Jayson Clark <jayson.clark0421@gmail.com>
2023-08-09 12:27:06 -0400

    Update

---
+++ html/temp.js
@@ -0,0 +1,75 @@
+window.onload = () => {
+    document.querySelector('footer').innerHTML += `
+    <div class="container pt-12 pt-lg-6 pb-13 pb-md-15">
+      <div class="row gy-6 gy-lg-0">
+        <div class="col-md-6 col-lg-4">
+          <div class="widget">
+            <!--  
+            <img class="mb-4" src="./assets/img/logo-light.png" srcset="./assets/img/logo-light@2x.png 2x" alt="" />
+			-->
+            <h1 class="display-1 text-white">
+              <font face="Praline-Regular">A Special Wish</font>
+            </h1>
+            <h4 class="display-4 text-gradient gradient-3 mb-4">
+              <font face="Praline-Regular">Ohio Valley Chapter</font>
+            </h4>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-6 col-lg-3">
+          <div class="widget">
+            <h1 class="widget-title text-white mb-3">
+              <font face="Praline-Regular">Get in Touch</font>
+            </h1>
+            <address>Post Office Box 674 <br> St. Clairsville, OH 43950</address>
+
+            <h4 class="widget-title text-white">Annmarie O'Grady</h4>
+            <p class="mb-0">Executive Director</p>
+            <a href="mailto:aogrady@spwishov.com" class="text-yellow">aogrady@spwishov.com</a>
+            <h2 class="widget-title pt-2"><a href="tel:7404492063">(740) 449-2063</a></h2>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-6 col-lg-3">
+          <div class="widget">
+            <h1 class="widget-title text-white mb-3">
+              <font face="Praline-Regular">Change a Life</font>
+            </h1>
+            <p class="mb-4 text-yellow">Help us change the lives of children suffering in the Ohio Valley.</p>
+            <p class="mb-5 text-violet">A Special Wish Foundation has been classified as a 501(c)(3) charitable
+              non-profit corporate.</p>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-6 col-lg-2">
+          <div class="widget">
+            <center>
+              <img class="img-fluid" width="70%" src="assets/img/give-ohio-final.png">
+              <p class="text-yellow" align="center">Give back to your local Ohio Charities</p>
+            </center>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+      </div>
+      <!--/.row -->
+      <hr class="mt-11 mb-5" />
+      <div class="d-md-flex align-items-center justify-content-between">
+        <p class="mb-2 mb-lg-0">© 2021 A Special Wish Ohio Valley. All rights reserved. <strong class="text-sky">Built
+            and Donated with <i class="fas fa-heart text-hot-red"></i> by Mathew Ward</strong></p>
+        <nav class="nav social social-white text-md-end">
+          <a href="#"><i class="uil uil-twitter"></i></a>
+          <a href="#"><i class="uil uil-facebook-f"></i></a>
+          <a href="#"><i class="uil uil-instagram"></i></a>
+          <a href="#"><i class="uil uil-youtube"></i></a>
+        </nav>
+        <!-- /.social -->
+      </div>
+      <!-- /div -->
+    </div>
+    <!-- /.container -->
+    `
+}
